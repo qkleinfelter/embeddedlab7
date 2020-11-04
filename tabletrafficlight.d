710390c495616ec653dc9374a77861d7da2d7746
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2020-11-03 20:23:36 -0500

    fix with gitignore

--- tabletrafficlight.d
+++
@@ -1,5 +0,0 @@
-.\tabletrafficlight.o: TableTrafficLight.c
-.\tabletrafficlight.o: TExaS.h
-.\tabletrafficlight.o: ..\inc\tm4c123gh6pm.h
-.\tabletrafficlight.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\tabletrafficlight.o: SysTick.h
